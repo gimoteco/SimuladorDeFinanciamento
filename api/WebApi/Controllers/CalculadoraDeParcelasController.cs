3f61d5003274b0a122a99bc86ed94fcc51280057
Guilherme Barbosa Ferreira <gimoteco@gmail.com>
2016-04-29 16:17:42 -0400

    Adição da entrada no servico de calculo de financiamento

--- api/WebApi/Controllers/CalculadoraDeParcelasController.cs
+++ api/WebApi/Controllers/CalculadoraDeParcelasController.cs
@@ -7,20 +7,21 @@ namespace WebApi.Controllers
     public class CalculadoraDeParcelasController: ApiController
     {
         [Route("api/calcularParcelas"), HttpGet]
-        public IHttpActionResult Calcular(string sistemaDeAmortizacao, decimal saldoDevedor, decimal taxaDeJuros, int prazo)
+        public IHttpActionResult Calcular(string sistemaDeAmortizacao, decimal entrada, decimal saldoDevedor, decimal taxaDeJuros, int prazo)
         {
             IReadOnlyList<Parcela> parcelas = null;
+            var saldoDevedorSemEntrada = saldoDevedor - entrada;
 
             switch (sistemaDeAmortizacao)
             {
                 case "Price":
-                    parcelas = SistemaDeAmortizacaoPrice.CalcularParcelas(saldoDevedor, taxaDeJuros, prazo);
+                    parcelas = SistemaDeAmortizacaoPrice.CalcularParcelas(saldoDevedorSemEntrada, taxaDeJuros, prazo);
                     break;
                 case "SAC":
-                    parcelas = SistemaDeAmortizacaoConstante.CalcularParcelas(saldoDevedor, taxaDeJuros, prazo);
+                    parcelas = SistemaDeAmortizacaoConstante.CalcularParcelas(saldoDevedorSemEntrada, taxaDeJuros, prazo);
                     break;
                 case "SAM":
-                    parcelas = SistemaDeAmortizacaoMisto.CalcularParcelas(saldoDevedor, taxaDeJuros, prazo);
+                    parcelas = SistemaDeAmortizacaoMisto.CalcularParcelas(saldoDevedorSemEntrada, taxaDeJuros, prazo);
                     break;
             }
 
